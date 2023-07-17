ef2c9fb1847b7d9e1fcd4a1a871246aa37e6d9fa
Shubhanjan Banerjee (363453) <116087109+shubhanjan-banerjee@users.noreply.github.com>
2023-07-17 11:30:04 +0530

    Basic web application running

---
+++ Code/ECommerceWeb/src/assets/env.js
@@ -0,0 +1,6 @@
+(function (window) {
+  window["env"] = window["env"] || {};
+
+  window["env"]["apiUrl"] = "http://localhost:3000/skill-tracker/api/v1/";
+  window["env"]["debug"] = true;
+})(this);
